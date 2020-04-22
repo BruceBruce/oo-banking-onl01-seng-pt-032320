d7ab3ff366def2519bed9fdef289e468ac015d2b
bruce willis <fashun_vyctum@yahoo.com>
2020-04-22 06:45:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,6 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @amount = 0
       @status = "complete"
     else
       @status = "rejected"
